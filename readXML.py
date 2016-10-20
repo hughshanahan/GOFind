4a1b02e1082963ad3218a7a161c55ea2b061490b
Hugh Shanahan <Hugh.Shanahan@physics.org>
2016-10-20 17:09:13 +0100

    Add files via upload
    Fixed issues with XML.

--- readXML.py
+++ readXML.py
@@ -22,37 +22,55 @@
 #  
 #  
 
-import sys, xmltodict
+import sys, xmltodict, urllib2
 
-def searchEntry(entryData,key):
+def searchEntry(entry,rootKey,key):
 	found = False
 	i = 0
 	value = ''
-	while (not found):
-		try: 
-			if (entryData[u'name'][i][u'@type'] == key):
-				value = entryData[u'name'][i]['#text']
-				found = True
-			break
-		except KeyError:
-			print entryData
+	if rootKey in entry: 
+		entryData = entry[rootKey]
+		while (not found):
+		    if type(entryData[u'name']) is list:
+			    eD = entryData[u'name'][i]
+		    else:
+			    eD = entryData[u'name']
+		    
+		    try: 
+			    if (eD[u'@type'] == key):
+				    value = eD['#text']
+				    found = True
+				    break
+		    except KeyError:
+			    print eD
 			
-		i += 1
-		if ( i >= len(entryData[u'name']) ):
-			found = True
+		    i += 1
+		    if ( i >= len(entryData[u'name']) ):
+			    found = True
 					 
 	return value	 
 	
 
 def main():
 	
-	with open('www.uniprot.org.xml') as fd:
-		doc = xmltodict.parse(fd.read())
+	urlUniprotFULL = 'http://www.uniprot.org/uniprot/?query=GO:0048364+AND+species:Arabidopsis%20thaliana&format=xml'
+	uniprotHandle = urllib2.urlopen(urlUniprotFULL)
+	uniprotData = uniprotHandle.read()
+	uniprotHandle.close()	
+	doc = xmltodict.parse(uniprotData)
+#	with open('www.uniprot.org.xml') as fd:
+#		doc = xmltodict.parse(fd.read())
 	uniProt = doc[u'uniprot']
 	entries = uniProt[u'entry']
 	for entry in entries:
-		species = searchEntry(entry[u'organism'],u'scientific')
-		locus = searchEntry(entry[u'gene'],u'ordered locus')
+		species = searchEntry(entry,u'organism',u'scientific')
+		try:
+			locus = searchEntry(entry,u'gene',u'ordered locus')
+			
+		except KeyError:
+			print u'gene'
+			print entry
+				
 		print species+' '+locus	
 
 	return 0
